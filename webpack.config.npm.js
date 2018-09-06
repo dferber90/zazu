ea7347d1e9f714b759aba4b065c235a28a33211a
Dominik Ferber <dominik.ferber@gmail.com>
2018-09-06 11:16:59 +0200

    chore: add multiple build targets

--- webpack.config.npm.js
+++ webpack.config.npm.js
@@ -3,14 +3,12 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const PeerDepsExternalsPlugin = require("peer-deps-externals-webpack-plugin");
 
-module.exports = {
+const baseConfig = {
   entry: path.join(__dirname, "src/index.js"),
   mode: "production",
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "index.js"
-  },
+  output: { path: path.join(__dirname, "dist") },
   module: {
     rules: [
       {
@@ -33,8 +31,59 @@ module.exports = {
       }
     ]
   },
-  plugins: [new CleanWebpackPlugin(["dist"])],
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    // Adds peerDependencies as externals
+    // See: https://webpack.js.org/guides/author-libraries/#externalize-lodash
+    new PeerDepsExternalsPlugin()
+  ],
   resolve: {
     extensions: [".js", ".jsx"]
   }
 };
+
+module.exports = [
+  {
+    ...baseConfig,
+    output: {
+      ...baseConfig.output,
+      library: "zazu",
+      // UMD: available after AMD or CommonJS
+      libraryTarget: "umd",
+      filename: "index.js"
+    }
+  },
+  {
+    ...baseConfig,
+    output: {
+      ...baseConfig.output,
+      library: "zazu",
+      // avilable as window.zazu
+      // available trough the window object, in the browser
+      libraryTarget: "window",
+      filename: "zazu.window.js"
+    }
+  },
+  {
+    ...baseConfig,
+    output: {
+      ...baseConfig.output,
+      library: "zazu",
+      // no idea what "this" is
+      //  available through the this object
+      libraryTarget: "this",
+      filename: "zazu.this.js"
+    }
+  },
+  {
+    ...baseConfig,
+    output: {
+      ...baseConfig.output,
+      library: "zazu",
+      // for script tags
+      // as a global variable made available by a script tag
+      libraryTarget: "var",
+      filename: "zazu.var.js"
+    }
+  }
+];
